0ab6e46a8ad418a647fe034aeaa113a932f794da
ricardo guerrero <richi_gro@hotmail.com>
2020-06-17 12:20:35 -0500

    code repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const App = () => {
+  
   return(
     <div>
       <h1>Hello</h1>
+      <img src="https://circuit-city-images.s3.us-east-2.amazonaws.com/1475248988-nintendo-game-boy.jpg"/>
     </div>
   );
 };
