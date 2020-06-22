2a112eddeec9ed0245bbf26f5f4c2938aec47740
ricardo guerrero <richi_gro@hotmail.com>
2020-06-22 16:51:18 -0500

    Deleted webpack config and replaced with several options for development and production it appears to reduce bundle size significantly

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+} else {
+    console.log('App running in production mode!');
+}
+
 ReactDOM.render(<Header/>, document.getElementById("roots"));
