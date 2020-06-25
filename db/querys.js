eb558ae98fddfb46912b746a3abee7ef55ad6075
ricardo guerrero <richi_gro@hotmail.com>
2020-06-25 14:34:33 -0500

    have an option to connect to local db

--- db/querys.js
+++ db/querys.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT,
-  database : process.env.RDS_DB_NAME
+  host     : process.env.RDS_HOSTNAME || 'localhost',
+  user     : process.env.RDS_USERNAME || 'root',
+  password : process.env.RDS_PASSWORD || 'duckDuckGoose',
+  port     : process.env.RDS_PORT || 3306,
+  database : process.env.RDS_DB_NAME || 'bestbuy_db'
 });
 
 connection.connect((err) => {
