378422a66c93f80404a82f3329d97f1db2c9a5a8
ricardo guerrero <richi_gro@hotmail.com>
2020-06-25 12:19:23 -0500

    added a database property to connection in mysql create connection

--- db/querys.js
+++ db/querys.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
   host     : process.env.RDS_HOSTNAME,
   user     : process.env.RDS_USERNAME,
   password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
+  port     : process.env.RDS_PORT,
+  database : process.env.port.RDS_DB_NAME
 });
 
 connection.connect((err) => {
