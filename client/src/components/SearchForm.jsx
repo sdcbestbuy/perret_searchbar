c1b9c515ea0b4d8daba19d77a852eaa1aa0f1c12
ricardo guerrero <richi_gro@hotmail.com>
2020-07-01 22:38:10 -0500

    making some progress on this bug

--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -16,6 +16,9 @@ const SearchBar = ({ suggestions }) => {
     //test
     const [selectedSuggestion, setSelectedSuggestion] = useState({});
 
+    //limit the size of the suggestion list
+    const suggestionsSizeLimit = 13; 
+
     const handleSubmit = (event) => {
       event.preventDefault();
       // remove before deploy
@@ -35,9 +38,10 @@ const SearchBar = ({ suggestions }) => {
         suggestion =>
           suggestion.product_name.toLowerCase().indexOf(userInput.toLowerCase()) > -1
       );
-      //limit the size of the suggestion list
-      if(filteredSuggestions1.length > 13) {
-        filteredSuggestions1 = filteredSuggestions1.slice(0, 14);
+      
+      //set a suggestion limit ammounnt to the size limmit
+      if ( filteredSuggestions1.length >  suggestionsSizeLimit ) {
+        filteredSuggestions1 = filteredSuggestions1.slice(0, suggestionsSizeLimit);
       }
       //initialize index
       setActiveSuggestions(0);
@@ -92,9 +96,12 @@ const SearchBar = ({ suggestions }) => {
     }
     else if (e.keyCode === 40) { //down arrow
       // key holding key to slect suggestions
-      if (activeSuggestions === filteredSuggestions.length - 1) {
+      console.log("active suggestions", activeSuggestions);
+      console.log("suggestionsSizeLimit", suggestionsSizeLimit);
+      if (activeSuggestions  === suggestionsSizeLimit - 1) {
         //if you reach the end of the index loop back to zero
-        setActiveSuggestions(0);
+        return;
+        // setActiveSuggestions(0);
       }
       //increment index by 1
       setActiveSuggestions(activeSuggestions + 1);
@@ -154,6 +161,7 @@ const SearchBar = ({ suggestions }) => {
           <div className="right-suggestion-container">
             <div className="dd-img-containers">
               <div>
+                {console.log("another test",filteredSuggestions[activeSuggestions])}
                 <img className="dd-img" src={ filteredSuggestions[activeSuggestions].thumbnailImage }/>
                 <p className="dd-name">{ filteredSuggestions[activeSuggestions].product_name }</p>
                 <p> { filteredSuggestions[activeSuggestions].regularPrice } </p>
